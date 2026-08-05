169d5959be2a0de560a71efa8c064873ad07a0db
ananya.b <ananya.b@tataelxsi.co.in>
2026-08-05 16:19:33 +0530

    test calculator

--- app/calculator.py
+++ app/calculator.py
@@ -1,12 +1,19 @@
-def multiply(a,b):
-    return a *b
- 
-def division(a,b):
-    if b==0:
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
         raise ValueError("cannot divide by 0")
-    return a/b
- 
+    return a / b
 
 if __name__ == "__main__":
     print("add result", add(10, 20))
     print("subtraction result", subtract(20, 5))
+    print("multiply result", multiply(10, 5))
+    print("division result", division(20, 5))
